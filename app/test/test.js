b01521e6cbdb189fff47e0fad6d2045ae97d5332
Amanda <aymikiluna@gmail.com>
2017-03-05 16:30:35 -0500

    Installed and configured Mocha, Chai, and Sinon.

---
+++ app/test/test.js
@@ -0,0 +1,7 @@
+var expect = require("chai").expect;
+
+describe("Test", function() {
+    it("does a thing", function() {
+        expect(true).to.equal(true);
+    });
+});
--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ config.database = {};
 
 config.server.port = 8080;
 
-config.database.url = 'http://localhost';
+config.database.url = "http://localhost";
 config.database.port = 3306;
 
 module.exports = config;
