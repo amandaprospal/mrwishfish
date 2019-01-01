92acc1f435a992f118a24b958555fca33e4cf3ed
Amanda <aymikiluna@gmail.com>
2019-01-01 14:37:03 -0500

    Implemented page that displays a wishlist.

--- client/src/components/dashboard/Wishlist.js
+++ client/src/components/dashboard/Wishlist.js
@@ -1,61 +1,75 @@
-import React from 'react';
+import React, {Component} from 'react';
+import auth0Client from '../../Auth';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import compose from 'recompose/compose';
 import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import WishlistItems from './WishlistItems';
+import {NavLink, withRouter} from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-import tileData from './tileData';
 
 const styles = theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    //backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: 600,
-    //height: 450,
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
+  button: {
+    margin: theme.spacing.unit,
+  }
 });
 
-function Wishlist(props) {
-  const { classes } = props;
+class Wishlist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        id: '',
+        name: ''
+    };
+  }
 
-  return (
-    <div className={classes.root}>
-      <GridList cellHeight={230} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
-          <ListSubheader component="div"></ListSubheader>
-        </GridListTile>
-        {tileData.map(tile => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              subtitle={<span>{tile.price}</span>}
-              actionIcon={
-                <IconButton className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
-  );
+  componentDidMount() {
+    this.getWishlist();
+  }
+
+  getWishlist() {
+    var wishlistID = this.props.wishlistId;
+    const _this = this;
+    var getWishlistEndpoint = 'http://' + process.env.REACT_APP_DOMAIN + ':8080/api/v1/wishlists/' + wishlistID;
+    axios.get(getWishlistEndpoint, {
+        headers: { 
+            'Authorization': `Bearer ${auth0Client.getIdToken()}`,
+            "accepts": "application/json"
+        }
+    })
+    .then(function (response) {
+        _this.setState({
+            id: response.data.wishlist.id,
+            name: response.data.wishlist.name
+        });
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+  }
+
+  render() {  
+    const {classes} = this.props;
+    return (
+        <React.Fragment>
+          <Typography variant="h4" gutterBottom component="h2">
+            {this.state.name}
+          </Typography>
+          <Button variant="contained" color="secondary" className={classes.button} component={NavLink} to={`/newwishlistitem/${this.props.wishlistId}`}>
+            Add New Item
+          </Button>
+          <WishlistItems wishlistId={this.props.wishlistId}/>
+        </React.Fragment>
+    );
+  }
+
 }
 
 Wishlist.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Wishlist);
+export default compose(
+withStyles(styles)
+)(withRouter(Wishlist));
